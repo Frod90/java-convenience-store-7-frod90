d04e1e7082faa9bbaed0a155ce7e8686b12524b6
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-09 17:28:44 +0900

    feat: 상품 생성 기능 구현

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,47 @@
+package store.domain;
+
+import java.util.Objects;
+
+public class Product {
+
+	private final String name;
+	private final int price;
+	private final Promotion promotion;
+
+	private Product(String name, int price, Promotion promotion) {
+		this.name = name;
+		this.price = price;
+		this.promotion = promotion;
+	}
+
+	public static Product create(String name, int price, Promotion promotion) {
+		return new Product(name, price, promotion);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public Promotion getPromotion() {
+		return promotion;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Product product = (Product)o;
+		return Objects.equals(name, product.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(name);
+	}
+}
