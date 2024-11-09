840ea46bdc320e2d3f795e07e25e7567afb6e9b9
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-09 13:22:03 +0900

    test: 파일 내용 읽는 기능 테스트 구현

---
+++ src/test/java/store/util/StoreFileReaderTest.java
@@ -0,0 +1,30 @@
+package store.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StoreFileReaderTest {
+
+	@DisplayName("정해진 입력 형식을 지킨 파일을 읽을 수 있다.")
+	@Test
+	void readWithoutHeader() {
+
+		// given
+		String filePath = "src/test/resources/test_promotions.md";
+
+		// when
+		List<String[]> result = StoreFileReader.readWithoutHeader(filePath);
+
+		// then
+		assertThat(result).hasSize(3)
+			.containsExactly(
+				new String[] {"frod promotion", "1", "1", "2024-01-01", "2024-02-31"},
+				new String[] {"roy promotion", "2", "1", "2024-02-01", "2024-03-31"},
+				new String[] {"hana promotion", "2", "2", "2024-03-01", "2024-04-30"}
+			);
+	}
+}
---
+++ src/test/resources/test_promotions.md
@@ -0,0 +1,4 @@
+name,buy,get,start_date,end_date
+frod promotion,1,1,2024-01-01,2024-02-31
+roy promotion,2,1,2024-02-01,2024-03-31
+hana promotion,2,2,2024-03-01,2024-04-30
