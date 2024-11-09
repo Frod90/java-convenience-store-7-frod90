6357cb581f3e0c5f80990bcf76c052e8e30388c4
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-09 11:10:34 +0900

    feat: 프로모션 생성 기능 구현
    프로모션 생성 기능 구현,
    null 대체할 프로모션 생성 기능 구현

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,44 @@
+package store.domain;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+
+	private static final LocalDateTime DEFAULT_DATE_TIME =
+		LocalDateTime.of(2000, 1, 1, 0, 0, 0);
+	private static final String NO_PROMOTION_NAME = "none";
+	private static final Promotion NONE_PROMOTION = new Promotion(
+		NO_PROMOTION_NAME,
+		0,
+		0,
+		DEFAULT_DATE_TIME,
+		DEFAULT_DATE_TIME
+	);
+
+	private final String name;
+	private final int buy;
+	private final int get;
+	private final LocalDateTime startDate;
+	private final LocalDateTime endDate;
+
+	private Promotion(String name, int buy, int get, LocalDateTime startDate, LocalDateTime endDate) {
+		this.name = name;
+		this.buy = buy;
+		this.get = get;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public static Promotion of(String name, int buy, int get, LocalDateTime startDate, LocalDateTime endDate) {
+		return new Promotion(name, buy, get, startDate, endDate);
+	}
+
+	public static Promotion getNoneInstance() {
+		return NONE_PROMOTION;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
