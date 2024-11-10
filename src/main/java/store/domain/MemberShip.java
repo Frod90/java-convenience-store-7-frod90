6671d078b0bb17247412bef95b469c05784655ae
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-11 00:10:26 +0900

    feat: 멤버쉽 기능 구현

---
+++ src/main/java/store/domain/MemberShip.java
@@ -0,0 +1,25 @@
+package store.domain;
+
+public class MemberShip {
+
+	private static final double DISCOUNT_RATE = 0.3;
+
+	private final boolean isActive;
+
+	public MemberShip(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public static MemberShip of(boolean isActive) {
+		return new MemberShip(isActive);
+	}
+
+	public double getDiscountRate() {
+		if(isActive) {
+			return DISCOUNT_RATE;
+		}
+
+		return 0;
+	}
+
+}
