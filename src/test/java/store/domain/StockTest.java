6c92f7803a4729a009d3fda1d2fd06c87abb2c47
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-11 08:12:43 +0900

    test: 재고 관련 테스트 구현

--- src/test/java/store/domain/StockTest.java
+++ src/test/java/store/domain/StockTest.java
@@ -1,6 +1,7 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
 import java.util.stream.Stream;
@@ -9,10 +10,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class StockTest {
+
+	private static Stream<Arguments> providePromotionForCheckingHasPromotion() {
+		return Stream.of(
+			Arguments.of(
+				Promotion.of("Frod Promotion", 1, 1,
+					LocalDateTime.of(2001, 4, 21, 0, 0),
+					LocalDateTime.of(2001, 5, 21, 0, 0)
+				),
+				true
+			),
+			Arguments.of(Promotion.getNoneInstance(), false)
+		);
+	}
+
+	private static Stream<Arguments> provideDateTimeForCheckingActivePromotion() {
+		return Stream.of(
+			Arguments.of(LocalDateTime.of(2001, 4, 20, 23, 59, 59), false),
+			Arguments.of(LocalDateTime.of(2001, 4, 21, 0, 0), true),
+			Arguments.of(LocalDateTime.of(2001, 5, 20, 23, 59, 59), true),
+			Arguments.of(LocalDateTime.of(2001, 5, 21, 0, 0, 0), false)
+		);
+	}
 
 	@DisplayName("재고를 생성할 수 있다.")
 	@Test
@@ -131,19 +155,6 @@ class StockTest {
 
 	}
 
-	private static Stream<Arguments> providePromotionForCheckingHasPromotion() {
-		return Stream.of(
-			Arguments.of(
-				Promotion.of("Frod Promotion", 1, 1,
-					LocalDateTime.of(2001, 4, 21, 0, 0),
-					LocalDateTime.of(2001, 5, 21, 0, 0)
-				),
-				true
-			),
-			Arguments.of(Promotion.getNoneInstance(), false)
-			);
-	}
-
 	@DisplayName("프로모션이 활성화된 상품을 가지고 있는지 확인할 수 있다.")
 	@MethodSource("provideDateTimeForCheckingActivePromotion")
 	@ParameterizedTest(name = "{0} 시각에 재고는 활성화된 상품을 가지고 있다는 것은 {1}이다")
@@ -165,13 +176,157 @@ class StockTest {
 		assertThat(result).isEqualTo(expect);
 	}
 
-	private static Stream<Arguments> provideDateTimeForCheckingActivePromotion() {
-		return Stream.of(
-			Arguments.of(LocalDateTime.of(2001, 4, 20, 23, 59, 59), false),
-			Arguments.of(LocalDateTime.of(2001, 4, 21, 0, 0), true),
-			Arguments.of(LocalDateTime.of(2001, 5, 20, 23, 59, 59), true),
-			Arguments.of(LocalDateTime.of(2001, 5, 21, 0, 0, 0), false)
+	@DisplayName("재고 수량을 차감할 수 있다.")
+	@ParameterizedTest(name = "{0} 시각에 {1}개를 구매하면 프로모션 재고는 {2}개가 남고, 일반 재고는 {3}개가 남는다.")
+	@CsvSource({
+		"2001-04-25T00:00:00, 5, 5, 10",
+		"2001-04-25T00:00:00, 15, 0, 5",
+		"2001-06-01T00:00:00, 5, 10, 5"
+	})
+	void deductQuantity(
+		LocalDateTime now, int quantity,
+		int expectedPromotionQuantity, int expectedGeneralQuantity
+	) {
+		// given
+		Promotion promotion = Promotion.of("Food Promotion", 1, 1,
+			LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0)
+		);
+
+		Product product = Product.create("제로 콜라", 2000, promotion);
+		Stock stock = Stock.from(product);
+		stock.incrementPromotionQuantity(10);
+		stock.incrementGeneralQuantity(10);
+
+		// when
+		stock.deductQuantity(quantity, now);
+
+		// then
+		assertAll(
+			() -> assertThat(stock.getPromotionQuantity()).isEqualTo(expectedPromotionQuantity),
+			() -> assertThat(stock.getGeneralQuantity()).isEqualTo(expectedGeneralQuantity)
 		);
 	}
 
+	@DisplayName("재고를 초과하여 차감하려고 할 때 예외가 발생한다.")
+	@ParameterizedTest(name = "{0}시각에 {1}개를 구매할 수 없다.")
+	@CsvSource({
+		"2001-04-25T00:00:00, 25",
+		"2001-06-01T00:00:00, 15"
+	})
+	void deductQuantityWithOverFlow(LocalDateTime now, int quantity) {
+		// given
+		Promotion promotion = Promotion.of("Food Promotion", 1, 1,
+			LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0)
+		);
+
+		Product product = Product.create("제로 콜라", 2000, promotion);
+		Stock stock = Stock.from(product);
+		stock.incrementPromotionQuantity(10);
+		stock.incrementGeneralQuantity(10);
+
+		// when & then
+		assertThatThrownBy(() -> stock.deductQuantity(quantity, now))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR]");
+	}
+
+	@DisplayName("프로모션 결과를 계산할 수 있다.")
+	@ParameterizedTest
+	@CsvSource({
+		"5, 2001-04-25T00:00:00, 2, 1, 0",
+		"15, 2001-04-25T00:00:00, 7, 1, 0"
+	})
+	void calculatePromotion(int purchasedQuantity, LocalDateTime openDateTime,
+		int expectedFreeQuantity, int expectedExtraQuantity, int expectedUnApplicableQuantity) {
+
+		// given
+		Promotion promotion = Promotion.of("Food Promotion", 1, 1,
+			LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0)
+		);
+
+		Product product = Product.create("제로 콜라", 2000, promotion);
+		Stock stock = Stock.from(product);
+		stock.incrementPromotionQuantity(20);
+		stock.incrementGeneralQuantity(20);
+
+		// when
+		PromotionResult result = stock.calculatePromotion(purchasedQuantity, openDateTime);
+
+		// then
+		assertThat(result)
+			.extracting("freeQuantity", "extraQuantity", "unApplicableQuantity")
+			.containsExactly(expectedFreeQuantity, expectedExtraQuantity, expectedUnApplicableQuantity);
+	}
+
+	@DisplayName("프로모션이 활성화되어 있지 않으면 프로모션을 계산할 수 없다.")
+	@Test
+	void calculatePromotionNotInTime() {
+
+		// given
+		Promotion promotion = Promotion.of("Food Promotion", 1, 1,
+			LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0)
+		);
+
+		Product product = Product.create("제로 콜라", 2000, promotion);
+		Stock stock = Stock.from(product);
+		stock.incrementPromotionQuantity(20);
+		stock.incrementGeneralQuantity(20);
+
+		LocalDateTime openDateTime = LocalDateTime.of(2001, 5, 21, 0, 0);
+
+		// when // then
+		assertThatThrownBy(() -> stock.calculatePromotion(5, openDateTime))
+			.isInstanceOf(IllegalStateException.class)
+				.hasMessageContaining("[ERROR]");
+	}
+
+	@DisplayName("프로모션 적용이 가능한지 확인할 수 있다.")
+	@CsvSource({
+		"2001-04-25T00:00:00, 10, true",
+		"2001-05-21T00:00:00, 10, false"
+	})
+	@ParameterizedTest
+	void hasActivePromotionProductAndExistPromotionQuantity(LocalDateTime openDateTime, int incrementQuantity, boolean expect) {
+
+		// given
+		Promotion promotion = Promotion.of("Food Promotion", 1, 1,
+			LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0)
+		);
+
+		Product product = Product.create("제로 콜라", 2000, promotion);
+		Stock stock = Stock.from(product);
+		stock.incrementPromotionQuantity(incrementQuantity);
+
+		// when
+		boolean result = stock.hasActivePromotionProductAndExistPromotionQuantity(openDateTime);
+
+		// then
+		assertThat(result).isEqualTo(expect);
+	}
+
+	@DisplayName("구매 수량이 총 재고 수량을 초과할 수 없다.")
+	@Test
+	void validateOverFlowPurchasedQuantity() {
+		// given
+		Promotion promotion = Promotion.of("Food Promotion", 1, 1,
+			LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0)
+		);
+
+		Product product = Product.create("제로 콜라", 2000, promotion);
+		Stock stock = Stock.from(product);
+		stock.incrementPromotionQuantity(5);
+		stock.incrementGeneralQuantity(7);
+
+		// when // then
+		assertThatThrownBy(() -> stock.validateOverFlowPurchasedQuantity(20))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR]");
+
+	}
 }
--- src/test/java/store/domain/StocksTest.java
+++ src/test/java/store/domain/StocksTest.java
@@ -1,6 +1,7 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
@@ -10,6 +11,8 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StocksTest {
 
@@ -18,44 +21,237 @@ class StocksTest {
 	void from() {
 
 		// given
-		Promotion promotion01 = Promotion.of("Frod Promotion", 1, 1,
-			LocalDateTime.of(2001, 4, 21, 0, 0),
-			LocalDateTime.of(2001, 5, 21, 0, 0)
-		);
-		Promotion promotion02 = Promotion.of("Roy Promotion", 1, 1,
-			LocalDateTime.of(2001, 5, 11, 0, 0),
-			LocalDateTime.of(2001, 6, 11, 0, 0)
-		);
-		Promotion promotion03 = Promotion.of("Hana Promotion", 1, 1,
-			LocalDateTime.of(2001, 7, 1, 0, 0),
-			LocalDateTime.of(2001, 8, 1, 0, 0)
-		);
+		Promotion promotion01 = Promotion.of("Frod Promotion", 1, 1, LocalDateTime.of(2001, 4, 21, 0, 0),
+			LocalDateTime.of(2001, 5, 21, 0, 0));
+		Promotion promotion02 = Promotion.of("Roy Promotion", 1, 1, LocalDateTime.of(2001, 5, 11, 0, 0),
+			LocalDateTime.of(2001, 6, 11, 0, 0));
+		Promotion promotion03 = Promotion.of("Hana Promotion", 1, 1, LocalDateTime.of(2001, 7, 1, 0, 0),
+			LocalDateTime.of(2001, 8, 1, 0, 0));
 
-		List<Product> products = List.of(
-			Product.create("제로 콜라", 2000, promotion01),
-			Product.create("아메리카노", 3000, promotion02),
-			Product.create("녹차라떼", 4000, promotion03)
-		);
+		List<Product> products = List.of(Product.create("제로 콜라", 2000, promotion01),
+			Product.create("아메리카노", 3000, promotion02), Product.create("녹차라떼", 4000, promotion03));
 
 		Map<String, Stock> inputStocks = products.stream()
-			.collect(Collectors.toMap(
-				Product::getName,
-				Stock::from,
-				(oldValue, newValue) -> oldValue,
-				LinkedHashMap::new
-			));
+			.collect(
+				Collectors.toMap(Product::getName, Stock::from, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
 		// when
 		Stocks stocks = Stocks.from(inputStocks);
 
 		// then
-		assertThat(stocks.getStocks())
-			.hasSize(3)
+		assertThat(stocks.getStocks()).hasSize(3)
 			.extractingFromEntries(Map.Entry::getValue)
-			.containsExactly(
-				inputStocks.get("제로 콜라"),
-				inputStocks.get("아메리카노"),
-				inputStocks.get("녹차라떼")
-			);
+			.containsExactly(inputStocks.get("제로 콜라"), inputStocks.get("아메리카노"), inputStocks.get("녹차라떼"));
+	}
+
+	@DisplayName("제품을 조회할 수 있다.")
+	@Test
+	void findAvailableProductBy() {
+
+		// given
+		Promotion promotion = Promotion.of("2+1 프로모션", 2, 1, LocalDateTime.of(2022, 1, 1, 0, 0),
+			LocalDateTime.of(2022, 12, 31, 23, 59));
+
+		Product product01 = Product.create("제로 콜라", 2000, promotion);
+		Product product02 = Product.create("오렌지 주스", 1500, Promotion.getNoneInstance());
+
+		Stock stock01 = Stock.from(product01);
+		Stock stock02 = Stock.from(product02);
+
+		stock01.incrementPromotionQuantity(10);
+		stock01.incrementGeneralQuantity(10);
+		stock02.incrementGeneralQuantity(10);
+
+		Map<String, Stock> stockMap = Map.of(
+			stock01.getProduct().getName(), stock01,
+			stock02.getProduct().getName(), stock02
+		);
+
+		Stocks stocks = Stocks.from(stockMap);
+
+		// when
+		Product foundProduct = stocks.findAvailableProductBy("제로 콜라", 5);
+
+		// then
+		assertThat(foundProduct.getName()).isEqualTo("제로 콜라");
+	}
+
+	@Test
+	@DisplayName("없는 제품을 조회할 수 없다.")
+	void findAvailableProductByWithoutProduct() {
+
+		// given
+		Promotion promotion = Promotion.of("2+1 프로모션", 2, 1,
+			LocalDateTime.of(2022, 1, 1, 0, 0),
+			LocalDateTime.of(2022, 12, 31, 23, 59)
+		);
+
+		Product product01 = Product.create("제로 콜라", 2000, promotion);
+		Product product02 = Product.create("오렌지 주스", 1500, Promotion.getNoneInstance());
+
+		Stock stock01 = Stock.from(product01);
+		Stock stock02 = Stock.from(product02);
+
+		stock01.incrementPromotionQuantity(10);
+		stock01.incrementGeneralQuantity(10);
+		stock02.incrementGeneralQuantity(10);
+
+		Map<String, Stock> stockMap = Map.of(
+			stock01.getProduct().getName(), stock01,
+			stock02.getProduct().getName(), stock02
+		);
+
+		Stocks stocks = Stocks.from(stockMap);
+
+		// when // then
+		assertThatThrownBy(() -> stocks.findAvailableProductBy("null", 5)).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR]");
+	}
+
+	@Test
+	@DisplayName("재고가 부족한 상품은 조회할 수 없다.")
+	void findAvailableProductByWithLackQuantity() {
+
+		// given
+		Promotion promotion = Promotion.of("2+1 프로모션", 2, 1,
+			LocalDateTime.of(2022, 1, 1, 0, 0),
+			LocalDateTime.of(2022, 12, 31, 23, 59)
+		);
+
+		Product product01 = Product.create("제로 콜라", 2000, promotion);
+		Product product02 = Product.create("오렌지 주스", 1500, Promotion.getNoneInstance());
+
+		Stock stock01 = Stock.from(product01);
+		Stock stock02 = Stock.from(product02);
+
+		Map<String, Stock> stockMap = Map.of(
+			stock01.getProduct().getName(), stock02,
+			stock02.getProduct().getName(), stock02
+		);
+
+		Stocks stocks = Stocks.from(stockMap);
+
+		// when // then
+		assertThatThrownBy(() -> stocks.findAvailableProductBy("제로 콜라", 5))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR]");
+	}
+
+	@DisplayName("프로모션 결과를 계산할 수 있다.")
+	@CsvSource({
+		"5, 1, 1, 0",
+		"15, 3, 0, 6"
+	})
+	@ParameterizedTest
+	void calculatePromotion(int purchasedQuantity, int expectedFreeQuantity, int expectedExtraQuantity,
+		int expectedUnApplicableQuantity) {
+
+		// given
+		Promotion promotion = Promotion.of("2+1 프로모션", 2, 1,
+			LocalDateTime.of(2022, 1, 1, 0, 0),
+			LocalDateTime.of(2022, 12, 31, 23, 59)
+		);
+
+		Product product01 = Product.create("제로 콜라", 2000, promotion);
+		Product product02 = Product.create("오렌지 주스", 1500, Promotion.getNoneInstance());
+
+		Stock stock01 = Stock.from(product01);
+		Stock stock02 = Stock.from(product02);
+
+		stock01.incrementPromotionQuantity(10);
+		stock01.incrementGeneralQuantity(10);
+		stock02.incrementGeneralQuantity(10);
+
+		Map<String, Stock> stockMap = Map.of(
+			stock01.getProduct().getName(), stock01,
+			stock02.getProduct().getName(), stock02
+		);
+
+		Stocks stocks = Stocks.from(stockMap);
+		LocalDateTime openDateTime = LocalDateTime.of(2022, 6, 1, 0, 0);
+
+		// when
+		PromotionResult promotionResult = stocks.calculatePromotion("제로 콜라", purchasedQuantity, openDateTime);
+
+		// then
+		assertThat(promotionResult)
+			.extracting("freeQuantity", "extraQuantity", "unApplicableQuantity")
+			.containsExactly(expectedFreeQuantity, expectedExtraQuantity, expectedUnApplicableQuantity);
+	}
+
+	@Test
+	@DisplayName("프로모션이 활성화 되어 있지 않으면 기본 프로모션 결과를 얻는다")
+	void calculatePromotionWithoutActive() {
+
+		// given
+		Promotion promotion = Promotion.of("2+1 프로모션", 2, 1,
+			LocalDateTime.of(2022, 1, 1, 0, 0),
+			LocalDateTime.of(2022, 12, 31, 23, 59)
+		);
+
+		Product productWithPromotion = Product.create("제로 콜라", 2000, promotion);
+		Product productWithoutPromotion = Product.create("오렌지 주스", 1500, Promotion.getNoneInstance());
+
+		Stock stockWithPromotion = Stock.from(productWithPromotion);
+		Stock stockWithoutPromotion = Stock.from(productWithoutPromotion);
+
+		stockWithPromotion.incrementPromotionQuantity(10);
+		stockWithPromotion.incrementGeneralQuantity(10);
+		stockWithoutPromotion.incrementGeneralQuantity(10);
+
+		Map<String, Stock> stockMap = Map.of(stockWithPromotion.getProduct().getName(), stockWithoutPromotion,
+			stockWithoutPromotion.getProduct().getName(), stockWithoutPromotion);
+
+		Stocks stocks = Stocks.from(stockMap);
+		LocalDateTime openDateTime = LocalDateTime.of(2022, 6, 1, 0, 0);
+
+		// when
+		PromotionResult promotionResult = stocks.calculatePromotion("오렌지 주스", 5, openDateTime);
+
+		assertThat(promotionResult).extracting("freeQuantity", "extraQuantity", "unApplicableQuantity")
+			.containsExactly(0, 0, 0);
+	}
+
+	@Test
+	@DisplayName("재고를 차감할 수 있다.")
+	void deductQuantity() {
+
+		// given
+		Promotion promotion = Promotion.of("2+1 프로모션", 2, 1, LocalDateTime.of(2022, 1, 1, 0, 0),
+			LocalDateTime.of(2022, 12, 31, 23, 59));
+
+		Product product01 = Product.create("제로 콜라", 2000, promotion);
+		Product product02 = Product.create("오렌지 주스", 1500, Promotion.getNoneInstance());
+
+		Stock stock01 = Stock.from(product01);
+		Stock stock02 = Stock.from(product02);
+
+		stock01.incrementPromotionQuantity(10);
+		stock01.incrementGeneralQuantity(10);
+		stock02.incrementGeneralQuantity(10);
+
+		Map<String, Stock> stockMap = Map.of(
+			stock01.getProduct().getName(), stock01,
+			stock02.getProduct().getName(), stock02
+		);
+
+		Stocks stocks = Stocks.from(stockMap);
+
+		Receipts receipts = Receipts.from(
+			List.of(
+				Receipt.of(stock01.getProduct(), 5, 2),  // 5개 구매, 2개 프로모션 적용
+				Receipt.of(stock02.getProduct(), 3, 0)  // 3개 구매, 프로모션 없음
+			),
+			MemberShip.of(false)
+		);
+
+		LocalDateTime now = LocalDateTime.of(2022, 6, 1, 0, 0);
+		stocks.deductQuantity(receipts, now);
+
+		assertAll(
+			() -> assertThat(stock01.getPromotionQuantity()).isEqualTo(5),
+			() -> assertThat(stock01.getGeneralQuantity()).isEqualTo(10),
+			() -> assertThat(stock02.getGeneralQuantity()).isEqualTo(7)
+		);
 	}
 }
